5ae860644649636107c6683f46394dccf8567336
dnabil <daffanabilhakim@gmail.com>
2023-01-30 14:44:51 +0700

    add test route
    bab: Setup framework (ping)

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+    // Membuat Gin Engine
+    r := gin.Default()
+
+    // Membuat route "/helloworld"
+    r.GET("/helloworld", func(c *gin.Context) {
+        // Mengirimkan string "hello world" sebagai response
+        c.String(200, "hello world")
+    })
+
+    // Menjalankan Gin Engine
+    r.Run("localhost:8080")
 }
