e27be6536700c29ac4d1deae08dc46bf2eea605e
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-08 15:54:26 +0700

    feat: add user entity

--- entity/user.go
+++ entity/user.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
+	Name     string `gorm:"type:VARCHAR(50); NOT NULL" json:"name"`
 	Username string `gorm:"type:VARCHAR(50); NOT NULL" json:"username"`
 	Password string `gorm:"type:TEXT; NOT NULL" json:"-"`
 	Posts    []Post `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"posts"`
