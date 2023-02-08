605ad5d4458c42475709db01545be102c3944c3d
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-08 15:55:38 +0700

    refactor: add new property and new struct

--- model/user_model.go
+++ model/user_model.go
@@ -1,6 +1,12 @@
 package model
 
 type RegisterUser struct {
+	Name     string `json:"name"`
 	Username string `json:"username"`
-	Password string `json:"-"`
+	Password string `json:"password"`
+}
+
+type LoginUser struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
